25a65a2c63f8fc0e99d50e56cc851c3404858bd9
Armando <armando.gonzalez@correo.unimet.edu.ve>
2022-11-12 16:55:06 -0600

    first commit

---
+++ 01-Control-Flow/Control-Flow/Control-Flow/Program-1.cs
@@ -0,0 +1,36 @@
+﻿
+
+void Main()
+{
+    Console.WriteLine("Type a number: ");
+    var inputUser = Console.ReadLine();
+
+    while (true)
+    {
+        // if inputUser is numeric
+        // I use this way because the homework use from 1 to 10
+        if (int.TryParse(inputUser, out _))
+        {
+            
+            if(Convert.ToInt32(inputUser) >=1 && Convert.ToInt32(inputUser) <= 10)
+            {
+                Console.WriteLine("Valid");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+                break;
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("Type a number: ");
+            inputUser = Console.ReadLine();
+        }
+
+    }
+}
+
+Main();
---
+++ 01-Control-Flow/Control-Flow/Control-Flow/Readme.md
@@ -0,0 +1,9 @@
+﻿
+
+1- Write a program and ask the user to enter a number. 
+The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. 
+Otherwise, display "Invalid". 
+(This logic is used a lot in applications where values entered into input boxes need to be validated.)
+
+
+
